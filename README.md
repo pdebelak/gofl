273435bfd86e6b6d205c6ac72ecc61f9bd52bc5c
pdebelak <pdebelak@gmail.com>
2015-08-23 19:48:29 -0500

    Update readme, decouple most controller specs from db

--- README.md
+++ README.md
@@ -6,13 +6,17 @@ Play Conway's Game of Life. See details on [wikipedia](https://en.wikipedia.org/
 
 1. `git clone` this repo
 2. `bundle`
-3. `rails s`
+3. `rake db:setup`
+4. `rails s`
 
 Go to `localhost:3000` to play.
 
 ## Run tests
 
-Right now all logic is in javascript. Run `rake test:js` to open [jasmine](http://jasmine.github.io/) specs in browser.
+Run `rake` to run ruby specs. I tried to decouple as many of the specs as reasonable
+from the database and rails in general.
+
+Run `rake test:js` to open [jasmine](http://jasmine.github.io/) specs in browser.
 
 ## Plans
 
--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -3,31 +3,38 @@ require "test_helper"
 class BoardsControllerTest < ActionController::TestCase
 
   def setup
-    Board.new(name: "Test Board", board: [[false],[true]]).publish
+    @board = Board.new(name: "Test Board", board: [[false],[true]])
+  end
+
+  def load_stubbed(&block)
+    Board.stub(:load, @board, &block)
   end
 
   def test_show_finds_board_by_slug
-    board = Board.load(name: "Test Board")
-    get :show, id: board.slug
-    assert_equal board.name, assigns(:board).name
-    assert_equal board.board, assigns(:board).board
+    @board.publish
+    get :show, id: @board.slug
+    assert_equal @board.name, assigns(:board).name
+    assert_equal @board.board, assigns(:board).board
   end
 
   def test_show_has_standard_layout
-    board = Board.load(name: "Test Board")
-    get :show, id: board.slug
-    assert_template layout: "layouts/application"
+    load_stubbed do
+      get :show, id: @board.slug
+      assert_template layout: "layouts/application"
+    end
   end
 
   def test_show_has_no_layout_with_xhr
-    board = Board.load(name: "Test Board")
-    xhr :get, :show, id: board.slug
-    assert_template layout: false
+    load_stubbed do
+      xhr :get, :show, id: @board.slug
+      assert_template layout: false
+    end
   end
 
   def test_find_redirects_to_show_route
-    board = Board.load(name: "Test Board")
-    post :find, slug: board.slug
-    assert_redirected_to board_path(board.slug)
+    load_stubbed do
+      post :find, slug: @board.slug
+      assert_redirected_to board_path(@board.slug)
+    end
   end
 end
